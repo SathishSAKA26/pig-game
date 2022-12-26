956ea72bed62c9257b1c3b4ab8fa2f2acba90cbf
sathish <2000sathishkumar.d@gmail.com>
2022-12-26 14:41:52 +0530

    add js

--- scripts/script.js
+++ scripts/script.js
@@ -8,8 +8,8 @@ const btnHold = document.getElementById('btn--hold');
 const player0El = document.getElementById('player--0');
 const player1El = document.getElementById('player--1');
 // score elements
-const score0El = document.getElementById('score--0');
-const score1El = document.getElementById('score--1');
+const score0El = document.getElementById('score-0');
+const score1El = document.getElementById('score-1');
 // current elements
 const curScore0El = document.getElementById('current--0');
 const curScore1El = document.getElementById('current--1');
