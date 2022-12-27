715b2ac5bb6f0e992cf9d25ea06464bad7d518d4
sathish <2000sathishkumar.d@gmail.com>
2022-12-27 16:18:09 +0530

    add  code

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,32 @@
-'use strict';
+"use strict";
 
 // selecting elements
-const btnNew = document.getElementById('btn--new');
-const btnRoll = document.getElementById('btn--roll');
-const btnHold = document.getElementById('btn--hold');
+const btnNew = document.getElementById("btn--new");
+const btnRoll = document.getElementById("btn--roll");
+const btnHold = document.getElementById("btn--hold");
 // player elements
-const player0El = document.getElementById('player--0');
-const player1El = document.getElementById('player--1');
+const player0El = document.getElementById("player--0");
+const player1El = document.getElementById("player--1");
 // score elements
-const score0El = document.getElementById('score-0');
-const score1El = document.getElementById('score-1');
+const score0El = document.getElementById("score-0");
+const score1El = document.getElementById("score-1");
 // current elements
-const curScore0El = document.getElementById('current--0');
-const curScore1El = document.getElementById('current--1');
+const curScore0El = document.getElementById("current--0");
+const curScore1El = document.getElementById("current--1");
 // dice elements
-const diceEl = document.getElementById('dice');
+const diceEl = document.getElementById("dice");
 
 // global variable
 let scores, currentScore, activePlayer, dice;
 
 // functions
 
+// init function
+function init() {
+  scores = 0;
+  currentScore = 0;
+  activePlayer = 0;
+  dice = 0;
 
+  
+}
